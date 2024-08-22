4763a10ee425c60358c4b3b89559de1961ad91a0
Philip Davis <philipdavis01@gmail.com>
2024-08-22 11:33:17 -0500

    Fill out addition api handles for dx_data_api

--- dx_data_api.py
+++ dx_data_api.py
@@ -12,6 +12,11 @@ class ExecArg:
     ub: tuple[int, ...]
     namespace:str = None
 
+@dataclass
+class DSRegHandle:
+    namespace: str
+    parameters: dict
+
 def _bounds_to_box(lb, ub):
     if len(lb) != len(ub):
         raise TypeError('lb and ub must have same dimensionality')
@@ -32,17 +37,24 @@ class DXDataAPI:
     def __init__(self, socket):
         self.socket = socket
     
+    def _req_url(self, url):
+        return(f'http://{self.socket}/{url}')
+
     def _put(self, url, data, files):
-        req_url=f'http://{self.socket}/{url}'
-        return(requests.put(req_url, data=data, files=files))
+        return(requests.put(self._req_url(url), data=data, files=files))
     
-    def _post(self, url, data, files):
-        req_url=f'http://{self.socket}/{url}'
-        return(requests.post(req_url, data=data, files=files))
+    def _post(self, url, data, files = None):
+        return(requests.post(self._req_url(url), data=data, files=files))
 
     def _post_json(self, url, json):
-        req_url=f'http://{self.socket}/{url}'
-        return(requests.post(req_url, json=json))
+        return(requests.post(self._req_url(url), json=json))
+
+    def _get(self, url):
+        return(requests.get(self._req_url(url)))
+
+    def _get_url_content(self, url):
+        response = self._get(url)
+        return(json.loads(response.content))
 
     def GetNDArray(self, name, version, lb, ub, nspace = None):
         box = _bounds_to_box(lb, ub)
@@ -56,7 +68,7 @@ class DXDataAPI:
         arr = np.ndarray(dims, dtype=dtype, buffer=response.content)
         return(arr)
 
-    def PutNDArray(self, name, version, offset, arr, nspace = None):
+    def PutNDArray(self, arr, name, version, offset, nspace = None):
         box = shape_to_box(arr.shape, offset)
         data = {'box': json.dumps(box)}
         files = {'data': arr.tobytes()}
@@ -81,6 +93,18 @@ class DXDataAPI:
         response = self._post(url, data, files)
         return(dill.loads(response.content))
 
+    def GetVars(self):
+        return(self._get_url_content('dspaces/var/'))
+
+    def GetVarObjs(self, name):
+        return(self._get_url_content(f'dspaces/var/{name}/'))
+
+    def Register(self, type, name, data):
+        url = f'dspaces/register/{type}/{name}'
+        response = self._post(url, json.dumps(data))
+        handle_dict = json.loads(response.content)
+        return(DSRegHandle(**handle_dict))  
+
 if  __name__ == "__main__":
     def test_fn(a, b):
         return(a+b)
@@ -89,12 +113,18 @@ if  __name__ == "__main__":
     print(a[2:5,7:9])
     print(b[4:7,1:3])
     print(a[2:5,7:9] + b[4:7,1:3])
-    conn = DXDataAPI('20.84.58.28:8000')
-    conn.PutNDArray('ex_api2', 1, (2,1), a)
-    conn.PutNDArray('ex_api3', 3, (1,0), b)
+    conn = DXDataAPI('127.0.0.1:8002')
+    conn.PutNDArray(a, 'ex_api2', 1, (2,1))
+    conn.PutNDArray(b, 'ex_api3', 3, (1,0))
     result = conn.Exec([
                     ExecArg('ex_api2', 1, (4, 8), (6, 9)),
                     ExecArg('ex_api3', 3, (5, 1), (7, 2))],
                     test_fn)
     print(result)
-
+    result = conn.GetVars()
+    print(result)
+    result = conn.GetVarObjs('ex_api3')
+    print(result)
+    data = {'bucket':'noaa-goes17', 'path':'ABI-L1b-RadM/2020/215/15/OR_ABI-L1b-RadM1-M6C04_G17_s20202151508255_e20202151508312_c20202151508354.nc'}
+    result = conn.Register('s3nc', 'foo', data)
+    print(result)
